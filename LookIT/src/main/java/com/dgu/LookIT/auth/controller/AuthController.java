ba51934c91eac931ee00300139e6821dab07c1e4
jouijae <kuda1390@dgu.ac.kr>
2025-04-30 00:26:58 +0900

    fix: login error

--- LookIT/src/main/java/com/dgu/LookIT/auth/controller/AuthController.java
+++ LookIT/src/main/java/com/dgu/LookIT/auth/controller/AuthController.java
@@ -1,17 +1,16 @@
 package com.dgu.LookIT.auth.controller;
 
+import com.dgu.LookIT.annotaion.UserId;
+import com.dgu.LookIT.auth.dto.request.SignUpRequest;
 import com.dgu.LookIT.auth.dto.response.KakaoUserInfoResponse;
+import com.dgu.LookIT.auth.service.AuthService;
 import com.dgu.LookIT.auth.service.KakaoService;
 import com.dgu.LookIT.global.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
@@ -21,12 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final KakaoService kakaoService;
+    private final AuthService authService;
 
-    @GetMapping("/callback")
-    public ResponseEntity<?> callback(@RequestParam("code") String code) {
-        String accessToken = kakaoService.getAccessTokenFromKakao(code);
-        KakaoUserInfoResponse userInfo = kakaoService.getUserInfo(accessToken);
-        log.info("User Info: {}", userInfo);
-        return new ResponseEntity<>(HttpStatus.OK);
+//    @GetMapping("/callback")
+//    public ResponseEntity<?> callback(@RequestParam("code") String code) {
+//        String accessToken = kakaoService.getAccessTokenFromKakao(code);
+//        KakaoUserInfoResponse userInfo = kakaoService.getUserInfo(accessToken);
+//        log.info("User Info: {}", userInfo);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+
+    @GetMapping("/api/auth/login/kakao")
+    public ResponseDto<?> login(@RequestParam("code") String code) {
+        return ResponseDto.ok(authService.login(code));
+    }
+
+    @PatchMapping("/api/sign-up")
+    public ResponseDto<?> signUp(@UserId Long userId, @RequestBody SignUpRequest signUpRequest) {
+        return ResponseDto.created(authService.signUp(userId, signUpRequest));
     }
 }
