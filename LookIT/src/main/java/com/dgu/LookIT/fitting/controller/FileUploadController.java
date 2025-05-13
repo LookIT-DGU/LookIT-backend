df05ffd022b1b199c0592da2a23a4536887a489d
jouijae <kuda1390@dgu.ac.kr>
2025-05-13 19:40:55 +0900

    feat: virtual-fitting

--- LookIT/src/main/java/com/dgu/LookIT/fitting/controller/FileUploadController.java
+++ LookIT/src/main/java/com/dgu/LookIT/fitting/controller/FileUploadController.java
@@ -1,7 +1,9 @@
 package com.dgu.LookIT.fitting.controller;
 
-import com.dgu.LookIT.fitting.service.S3FileUploadService;
+import com.dgu.LookIT.annotaion.UserId;
+import com.dgu.LookIT.fitting.service.S3FileService;
 import com.dgu.LookIT.global.ResponseDto;
+import com.dgu.LookIT.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,18 @@ import java.io.IOException;
 @RequestMapping("")
 @RequiredArgsConstructor
 public class FileUploadController {
-    private final S3FileUploadService s3FileUploadService;
+    private final S3FileService s3FileService;
 
     @PostMapping("/upload")
     public ResponseDto<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        String url = s3FileUploadService.uploadFile(file);
+        String url = s3FileService.uploadFile(file);
         return ResponseDto.ok(url);
+    }
+
+    @PostMapping("/api/v0/virtual-fitting")
+    public ResponseDto<?> fitClothes(@UserId Long userId, @RequestParam("clothes") MultipartFile clothesImage
+    ) throws IOException {
+        String fittedImageUrl = s3FileService.processFitting(userId, clothesImage);
+        return ResponseDto.ok(fittedImageUrl);
     }
 }
